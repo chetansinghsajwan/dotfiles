89d0616e14d3d4b08919c3a21340440deb5a450f
shifu-dev <csingh8476@gmail.com>
2023-12-13 17:30:38 +0530

    add nix/home-manager/home.nix.

---
+++ nix/home-manager/home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "chetan";
+  home.homeDirectory = "/home/chetan";
+  home.stateVersion = "23.11";
+
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    vim
+    vscode
+    git
+    firefox
+    # obsidian
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {};
+
+  programs.home-manager.enable = true;
+}
