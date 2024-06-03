528fae8f65dffc2628655728ad7d263718685b93
shifu-dev <csingh8476@gmail.com>
2024-06-03 19:55:34 +0530

    move vlc config into vlc.nix.

--- home/packages/vlc.nix
+++ home/packages/vlc.nix
@@ -1,8 +1,27 @@
 { config, pkgs, ... }:
+let
+    vlcrcFile = "${config.home.homeDirectory}/.config/vlc/vlcrc";
+in
 {
-    home.packages = [
-        pkgs.vlc
+    home.packages = with pkgs; [
+        vlc
     ];
 
-    home.file."${config.home.homeDirectory}/.config/vlc/vlcrc".source = ./vlcrc;
+    home.file.${vlcrcFile}.text = ''
+        qt-system-tray=0
+        qt-video-autoresize=0
+        qt-privacy-ask=0
+        freetype-font=Noto Sans
+        aout=any
+        audio-filter=normvol
+        snapshot-path=images/vlc/screenshots
+        vout=any
+        audio-language=eng
+        sub-language=eng
+        vod-server=any
+        metadata-network-access=1
+        short-jump-size=5
+        medium-jump-size=10
+        long-jump-size=60
+    '';
 }
