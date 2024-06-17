08b7e79f36373a57401dc600daeb48f91aff84f3
shifu-dev <csingh8476@gmail.com>
2024-06-17 20:44:03 +0530

    add cmake tasks.

--- home/packages/vscode/features/cmake.nix
+++ home/packages/vscode/features/cmake.nix
@@ -5,27 +5,69 @@
     ninja
   ];
 
-  programs.vscode = {
-    extensions = with pkgs.vscode-extensions; [
-      twxs.cmake
-      ms-vscode.cmake-tools
-    ];
+  programs.vscode =
+    let
+      buildDir = "\${workspaceFolder}/build";
+      installDir = "\${workspaceFolder}/install";
+    in
+    {
+      extensions = with pkgs.vscode-extensions; [
+        twxs.cmake
+        ms-vscode.cmake-tools
+      ];
 
-    userSettings.cmake = {
-      cmakePath = "${pkgs.cmake}/bin/cmake";
-      sourceDirectory = "\${workspaceFolder}";
-      buildDirectory = "\${workspaceFolder}/build";
-      installPrefix = "\${workspaceFolder}/install";
-      copyCompileCommands = "\${workspaceFolder}/build/compile_commands.json";
-      autoSelectActiveFolder = true;
-      loadCompileCommands = true;
-      configureOnEdit = false;
-      configureOnOpen = false;
-      saveBeforeBuild = true;
-      buildBeforeRun = true;
-      revealLog = "focus";
-      skipConfigureIfCachePresent = true;
-      preferredGenerators = [ "Ninja" "Unix MakeFiles" ];
+      userSettings.cmake = {
+        cmakePath = "${pkgs.cmake}/bin/cmake";
+        sourceDirectory = "\${workspaceFolder}";
+        buildDirectory = buildDir;
+        installPrefix = installDir;
+        autoSelectActiveFolder = true;
+        loadCompileCommands = true;
+        # copyCompileCommands = "${buildDir}/compile_commands.json";
+        configureOnEdit = false;
+        configureOnOpen = false;
+        saveBeforeBuild = true;
+        buildBeforeRun = true;
+        revealLog = "focus";
+        skipConfigureIfCachePresent = true;
+        preferredGenerators = [ "Ninja" "Unix MakeFiles" ];
+      };
+
+      userTasks.tasks = [
+        {
+          label = "cmake build all";
+          command = "cmake --build ${buildDir}";
+          problemMatcher = [ ];
+        }
+        {
+          label = "cmake build target";
+          command = "cmake --build ${buildDir} --target \${input:cmakeTarget}";
+          problemMatcher = [ ];
+        }
+        {
+          label = "cmake clean";
+          command = "cmake --build build --target clean";
+          problemMatcher = [ ];
+        }
+        {
+          label = "cmake run";
+          command = "\${command:cmake.launchTargetPath}";
+          type = "process";
+          problemMatcher = [ ];
+        }
+        {
+          label = "cmake debug";
+          command = "\${command:workbench.action.debug.selectandstart}";
+          args = [
+            "cmake debug"
+          ];
+          problemMatcher = [ ];
+        }
+        {
+          label = "cmake install";
+          command = "cmake --install ${buildDir} --install-prefix ${installDir}";
+          problemMatcher = [ ];
+        }
+      ];
     };
-  };
 }
