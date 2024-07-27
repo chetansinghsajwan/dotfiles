46d876e97dccc82968deadd20aba939c45e86a37
shifu-dev <csingh8476@gmail.com>
2024-07-28 01:38:52 +0530

    updated system

--- hosts/workstation/configuration.nix
+++ hosts/workstation/configuration.nix
@@ -47,7 +47,6 @@ inputs:
     services.printing.enable = true;
 
     # Enable sound with pipewire.
-    sound.enable = true;
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
