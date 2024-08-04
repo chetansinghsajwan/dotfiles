15c7747989b3e1d504997e18ec157504384637aa
shifu-dev <csingh8476@gmail.com>
2024-08-04 16:39:21 +0530

    improve neovim config.

--- home/packages/neovim.nix
+++ home/packages/neovim.nix
@@ -3,18 +3,11 @@
   programs.neovim = {
     enable = true;
 
-    extraConfig = ''
-      nnoremap i <Up>
-      nnoremap k <Down>
-      nnoremap j <Left>
-      nnoremap l <Right>
-      nnoremap h <insert>
-
-      vnoremap i <Up>
-      vnoremap k <Down>
-      vnoremap j <Left>
-      vnoremap l <Right>
-      vnoremap h <insert>
+    extraLuaConfig = ''
+       vim.o.tabstop = 4
+       vim.o.expandtab = true
+       vim.o.softtabstop = 4
+       vim.o.shiftwidth = 4
     '';
   };
 }
