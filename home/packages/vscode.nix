f03148cbb7b01831fa6e580aea40cd5e34e88856
shifu-dev <csingh8476@gmail.com>
2024-09-28 01:44:52 +0530

    dont manage vscode config using home-manager

--- home/packages/vscode.nix
+++ home/packages/vscode.nix
@@ -1,32 +1,6 @@
 { pkgs, ... }:
 {
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscodium-fhs;
-    enableUpdateCheck = false;
-    enableExtensionUpdateCheck = false;
-    mutableExtensionsDir = false;
-
-    userSettings = import ./vscode/settings.nix;
-    userTasks = import ./vscode/tasks.nix;
-    keybindings = import ./vscode/keybindings.nix;
-  };
-
-  imports = [
-    ./vscode/languages/cpp.nix
-    ./vscode/languages/c.nix
-    ./vscode/languages/csharp.nix
-    ./vscode/languages/md.nix
-    ./vscode/languages/html.nix
-    ./vscode/languages/json.nix
-    ./vscode/languages/nix.nix
-
-    ./vscode/features/clangd.nix
-    ./vscode/features/cmake.nix
-    ./vscode/features/lldb.nix
-
-    ./vscode/themes/material-icons.nix
-    ./vscode/themes/one-dark-pro.nix
-    ./vscode/themes/github.nix
-  ];
+    home.packages = with pkgs; [
+        vscode
+    ];
 }
