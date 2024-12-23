0a91bc29d37880df9d22c2dda853c016fa282753
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-12-24 00:22:59 +0530

    update git config

--- home/packages/git.nix
+++ home/packages/git.nix
@@ -1,35 +1,59 @@
 { config, pkgs, ... }:
 {
-    home.packages = [ pkgs.git ];
+  home.packages = [ pkgs.git ];
 
-    programs.git = {
-        enable = true;
-        userName = "shifu-dev";
-        userEmail = "csingh8476@gmail.com";
+  programs.git = {
+    enable = true;
 
-        lfs = {
-            enable = true;
-            skipSmudge = false;
-        };
+    includes = [{ path = "~/.gitconfig.local"; }];
 
-        extraConfig = {
-            core.editor = "nvim";
-            credential.helper = "store";
-
-            advice.pushUpdateRejected = false;
-            advice.pushNonFFCurrent = false;
-            advice.pushNonFFMatching = false;
-            advice.pushAlreadyExists = false;
-            advice.pushFetchFirst = false;
-            advice.pushNeedsForce = false;
-            advice.statusHints = false;
-            advice.statusUoption = false;
-            advice.commitBeforeMerge = false;
-            advice.resolveConflict = false;
-            advice.implicitIdentity = false;
-            advice.detachedHead = false;
-            advice.amWorkDir = false;
-            advice.rmHints = false;
-        };
+    lfs = {
+      enable = true;
+      skipSmudge = false;
     };
+
+    extraConfig = {
+      core.editor = "nvim";
+      credential.helper = "store";
+      init.defaultBranch = "main";
+      push.default = "current";
+      pull.ff = "only";
+      pull.rebase = true;
+      protocol.version = 2;
+
+      # user settings
+      user.name = "chetansinghsajwan";
+      user.email = "76040441+chetansinghsajwan@users.noreply.github.com";
+
+      # url aliases
+      url."git@github.com:".insteadOf = "gh:";
+
+      # aliases
+      alias.cm = "commit";
+      alias.co = "checkout";
+      alias.br = "branch";
+      alias.st = "status";
+      alias.sreset = "reset --soft";
+      alias.hreset = "reset --hard";
+      alias.ammend = "commit --amend --all --no-edit";
+      alias.df = "diff --color --color-words --abbrev";
+      alias.lg = "log --color --oneline --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit";
+
+      # advice settings
+      advice.pushUpdateRejected = false;
+      advice.pushNonFFCurrent = false;
+      advice.pushNonFFMatching = false;
+      advice.pushAlreadyExists = false;
+      advice.pushFetchFirst = false;
+      advice.pushNeedsForce = false;
+      advice.statusHints = false;
+      advice.statusUoption = false;
+      advice.commitBeforeMerge = false;
+      advice.resolveConflict = false;
+      advice.implicitIdentity = false;
+      advice.detachedHead = false;
+      advice.amWorkDir = false;
+      advice.rmHints = false;
+    };
+  };
 }
