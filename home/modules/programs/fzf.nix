4a3ef51be70a43e1f6a72e48ce8365647d73b3a6
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2026-09-03 20:53:07 +0530

    improve fzf config

--- home/modules/programs/fzf.nix
+++ home/modules/programs/fzf.nix
@@ -1,6 +1,19 @@
 { config, ... }: {
   programs.fzf = {
+    enableBashIntegration = config.dotfiles.shell.program == "bash";
     enableZshIntegration = config.dotfiles.shell.program == "zsh";
     enableFishIntegration = config.dotfiles.shell.program == "fish";
+
+    defaultOptions = [
+      "--height 40%"
+      "--multi"
+      "--cycle"
+      "--scheme path"
+      "--preview 'bat --color=always --line-range :50 {} 2>/dev/null || ls -lah {}'"
+      "--bind 'ctrl-d:preview-page-down,ctrl-u:preview-page-up'"
+      "--bind 'ctrl-/:toggle-preview'"
+    ];
   };
+
+  programs.bat.enable = true;
 }
