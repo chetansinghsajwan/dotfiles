88c9bbf01aeadfabe7f4b35328372621af896a09
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2026-09-22 13:59:26 +0530

    Revert "yazi: show full cwd as the files pane's border title, not the header"
    This reverts commit 8a8f2b2f7043b09965d523b35763e8af48c431ec.

--- home/modules/programs/yazi.nix
+++ home/modules/programs/yazi.nix
@@ -11,6 +11,7 @@
     shellWrapperName = "y";
 
     plugins = {
+      full-border = pkgs.yaziPlugins.full-border;
       bookmarks = pkgs.yaziPlugins.bookmarks;
       properties = ./yazi/properties.yazi;
       places = ./yazi/places.yazi;
